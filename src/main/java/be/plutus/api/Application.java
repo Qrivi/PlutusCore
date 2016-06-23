d71273f0051240dbfa069da1adab1d77234821f1
Kristof Dewilde <me@krivi.be>
2016-06-23 16:20:42 +0200

    Added empty Spring Boot project

---
+++ src/main/java/be/plutus/api/Application.java
@@ -0,0 +1,14 @@
+package be.plutus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication( scanBasePackages = "be.plutus.api" )
+public class Application {
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( Application.class, args );
+    }
+
+}
