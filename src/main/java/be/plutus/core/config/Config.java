e5abb5c566e5f8ab1ee2f0910abda33746bc651e
Kristof Dewilde <me@krivi.be>
2016-07-10 03:25:09 +0200

    Added defaults and fixed default token TTL

--- src/main/java/be/plutus/core/config/Config.java
+++ src/main/java/be/plutus/core/config/Config.java
@@ -1,7 +1,13 @@
 package be.plutus.core.config;
 
+import be.plutus.core.model.currency.Currency;
+
 public class Config{
 
-    public static final long DEFAULT_TOKEN_TTL = new Long( "5259492000" );
+    public static final long DEFAULT_TOKEN_TTL = new Long( "5184000000" ); // 60 days
 
+    public static final Currency DEFAULT_CURRENCY = Currency.EUR;
+
+    public static final int DEFAULT_TRANSACTION_OFFSET = 0;
+    public static final int DEFAULT_TRANSACTION_LIMIT = 50;
 }
