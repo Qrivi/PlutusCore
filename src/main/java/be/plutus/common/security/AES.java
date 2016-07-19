e21af1fb203daa06c14296d4afe51764baef272a
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-07-19 22:17:03 +0200

    Added AES class for User password encrypting and decrypting

---
+++ src/main/java/be/plutus/common/security/AES.java
@@ -0,0 +1,61 @@
+package be.plutus.common.security;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import be.plutus.core.config.Config;
+import org.apache.commons.codec.binary.Base64;
+
+public class AES{
+
+    private static SecretKeySpec secretKey;
+
+    public static void setKey( String key ){
+        MessageDigest mDigest;
+        try {
+            byte[] keyBytes = key.getBytes( "UTF-8" );
+            mDigest = MessageDigest.getInstance("SHA-1");
+            keyBytes = mDigest.digest( keyBytes );
+            keyBytes = Arrays.copyOf( keyBytes, 16);
+            secretKey = new SecretKeySpec( keyBytes, "AES");
+        } catch ( Exception ignored ) {}
+    }
+
+    public static String encrypt( String toEncrypt )
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.encodeBase64String(cipher.doFinal( toEncrypt.getBytes("UTF-8")));
+        }
+        catch ( Exception ignored ){
+            return null;
+        }
+    }
+
+    public static String decrypt(String toDecrypt )
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.decodeBase64( toDecrypt )));
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        AES.setKey( Config.SECRET_KEY );
+        String encrypted = AES.encrypt( "this-is-a-password" );
+        String decrypted = AES.decrypt( encrypted );
+
+        System.out.println( "Encrypted : " + encrypted );
+        System.out.println( "Decrypted : " + decrypted );
+    }
+}
--- src/main/java/be/plutus/core/config/Config.java
+++ src/main/java/be/plutus/core/config/Config.java
@@ -8,6 +8,13 @@ public class Config{
 
     public static final Currency DEFAULT_CURRENCY = Currency.EUR;
 
+    public static final String SECRET_KEY = "ey4d2kb5lqqpqk2nrqhh";
+
+    /**
+     * TODO: use these default values (ctrl-click)
+     * @see be.plutus.core.service.TransactionService#getTransactionByUser
+     * @see be.plutus.core.service.TransactionService#getTransactionByUserAndType
+     */
     public static final int DEFAULT_TRANSACTION_OFFSET = 0;
     public static final int DEFAULT_TRANSACTION_LIMIT = 50;
 }
