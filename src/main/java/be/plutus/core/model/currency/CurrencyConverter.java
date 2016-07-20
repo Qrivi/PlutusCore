31d888846a5de36b6c25f130a47c1d06c2921c6f
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-07-21 00:52:46 +0200

    CurrencyConverter now rounds to 2 decimal places

--- src/main/java/be/plutus/core/model/currency/CurrencyConverter.java
+++ src/main/java/be/plutus/core/model/currency/CurrencyConverter.java
@@ -4,6 +4,7 @@ public class CurrencyConverter{
 
     public static double convert( double value, Currency from, Currency to ){
         double valueInEuro = value * from.getValueInEuro();
-        return valueInEuro / to.getValueInEuro();
+        double converted = valueInEuro / to.getValueInEuro();
+        return (double) Math.round(converted * 100) / 100;
     }
 }
